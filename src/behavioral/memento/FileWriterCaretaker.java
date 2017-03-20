10418aaefd1986eaf16be269ccf508239a3abb21
gonzaloJC <gonzalo.garay@appdirect.com>
2017-03-20 18:21:44 -0300

    add more patterns.

---
+++ src/behavioral/memento/FileWriterCaretaker.java
@@ -0,0 +1,17 @@
+package behavioral.memento;
+
+/**
+ * Created by gonzaloGar on 20/03/17.
+ */
+public class FileWriterCaretaker {
+
+    private Object obj;
+
+    public void save(FileWriterUtil fileWriter) {
+        this.obj = fileWriter.save();
+    }
+
+    public void undo(FileWriterUtil fileWriter) {
+        fileWriter.undoToLastSave(obj);
+    }
+}
